93ba3de64352e72154d473a1ac60ee97fd9ce712
Jennelle <nystromj@grinnell.edu>
2014-03-28 17:03:29 -0500

    change to auteematic.com

--- config/env/production.js
+++ config/env/production.js
@@ -1,12 +1,12 @@
 module.exports = {
     db: 'mongodb://root:root@oceanic.mongohq.com:10065/app23251161',
     app: {
-        name: 'Apteetude'
+        name: 'Auteematic'
     },
     facebook: {
         clientID: '680519565341022',
         clientSecret: '2091a2c3f5ae9933a82ba538ec5bf281',
-        callbackURL: 'http://rocky-waters-6979.herokuapp.com/auth/facebook/callback'
+        callbackURL: 'http://auteematic.com/auth/facebook/callback'
     },
     twitter: {
         clientID: 'CONSUMER_KEY',
