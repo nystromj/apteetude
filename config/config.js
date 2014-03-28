bf6bb371677f999e7f8e55970f1cf15e3d1be54e
Jennelle <nystromj@grinnell.edu>
2014-03-28 14:20:20 -0500

    fix config file

--- config/config.js
+++ config/config.js
@@ -4,6 +4,6 @@ var _ = require('lodash');
 // Extend the base configuration in all.js with environment
 // specific configuration
 module.exports = _.extend(
-    require(__dirname + '/../env/all.js'),
-    require(__dirname + '/../env/' + process.env.NODE_ENV + '.js') || {}
+    require(__dirname + '/../config/env/all.js'),
+    require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.js') || {}
 );
