566ce8fa34945469e491632f9f424c2da9a7dddc
Graeme Boy <graemeboy@gmail.com>
2014-03-28 13:31:37 -0500

    Add my views

---
+++ app/controllers/example.js
@@ -0,0 +1,28 @@
+var example = function (req, res) {
+  var redirectTo = req.session.returnTo ? req.session.returnTo : '/'
+  delete req.session.returnTo
+  res.redirect(redirectTo)
+}
+
+/*
+	Show Paul's example page
+*/
+
+
+exports.paul = function (req, res) {
+  res.render('example/paul', {
+    title: 'Login',
+    message: req.flash('error')
+  })
+}
+
+/*
+	Show Jessica's example page
+*/
+
+exports.jessica = function (req, res) {
+  res.render('example/jessica', {
+    title: 'Login',
+    message: req.flash('error')
+  })
+}
