bad8ac211857a39fcc840ac2cb808440be418e7e
Jennelle <nystromj@grinnell.edu>
2014-03-29 20:27:51 -0500

    add design conditionals

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -24,11 +24,15 @@ var parse_properties = function(properties) {
 
 var render_designs = function (user, properties) {
 	var results = []
-	results.push("Hello my name is " + user.facebook.first_name);
-  results.push("Dreaming of " + properties.College.meta.short_name);
-  results.push(properties.concentration.name + " Nerd");
-  results.push("I <3 " + properties.work.name);
-  results.push("Ask me about " + properties.concentration.name);
+	if (user.facebook.first_name)
+		results.push("Hello my name is " + user.facebook.first_name);
+	if (properties.College)
+		results.push("Dreaming of " + properties.College.meta.short_name);
+	if (properties.concentration)
+		results.push(properties.concentration.name + " Nerd");
+		results.push("Ask me about " + properties.concentration.name);
+	if (properties.work)
+		results.push("I <3 " + properties.work.name);
 	return results
 }
 
--- html/README.md
+++
@@ -1,9 +0,0 @@
-<h2>HTML Directory</h2>
-
-I have made this directory to put the static html that I'm using to design the store front. Alex made the mockup in photoshop, and I'm coding it in static html here.
-
-Later, we will use this template and add our JS functionality.
-
-You can see a live preview of this template here: http://graemeboy.com/apteetude/html/
-
-Feel free to comment on it as we progress in this phase of static template development.
